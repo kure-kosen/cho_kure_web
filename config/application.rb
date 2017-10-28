62811ecaa2663c15e0d925281e1f236ce0481729
Shintani Teppei <teppest1215@gmail.com>
2017-10-28 20:06:39 +0900

    rspec setup

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,16 @@ module ChoKureWeb
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
