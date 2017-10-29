f7a9cebc495f36201ff11ed3e8bebc9693a6b63d
Shintani Teppei <teppest1215@gmail.com>
2017-10-29 22:17:07 +0900

    readmeに追記

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,6 @@ module ChoKureWeb
     end
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
