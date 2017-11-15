40cb4a350a96ca9378c831e0c473fffd0885fab9
Shintani Teppei <teppest1215@gmail.com>
2017-11-15 10:20:47 +0900

    bundle exec rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module ChoKureWeb
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
 
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,5 @@
-Time::DATE_FORMATS[:default] = '%Y年%m月%d日 %H:%M'
-Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
-Time::DATE_FORMATS[:date] = '%Y/%m/%d'
-Time::DATE_FORMATS[:time] = '%H:%M:%S'
-Date::DATE_FORMATS[:default] = '%Y/%m/%d'
+Time::DATE_FORMATS[:default] = "%Y年%m月%d日 %H:%M"
+Time::DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
+Time::DATE_FORMATS[:date] = "%Y/%m/%d"
+Time::DATE_FORMATS[:time] = "%H:%M:%S"
+Date::DATE_FORMATS[:default] = "%Y/%m/%d"
