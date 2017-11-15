0ecc5a72cd36831734be1f48cec174aaa7b19c03
Shintani Teppei <teppest1215@gmail.com>
2017-11-15 22:04:14 +0900

    feedを消した

--- app/services/podcast/generate_feed.rb
+++ app/services/podcast/generate_feed.rb
@@ -7,35 +7,37 @@ module Podcast
     def initialize
       feed = RSS::Maker.make("2.0") do |feed|
 
-        feed.channel.title = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
-        feed.channel.link = "http://www.kure-rad.io/feed/podcast"
-        feed.channel.description = "〜呉・呉高専の今をお伝えします〜"
-        feed.channel.lastBuildDate = Time.now
-        feed.channel.sy_updatePeriod = "hourly"
-        feed.channel.sy_updateFrequency = "1"
+        channel = feed.channel
+        channel.title = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
+        channel.link = "http://www.kure-rad.io/feed/podcast"
+        channel.description = "〜呉・呉高専の今をお伝えします〜"
+        channel.lastBuildDate = Time.now
+        channel.sy_updatePeriod = "hourly"
+        channel.sy_updateFrequency = "1"
 
-        feed.image.url = "http://www.kure-rad.io/wp-content/uploads/2017/11/cropped-sumika-ブルー逆-32x32.jpg"
-        feed.image.title = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
-        feed.image.width = "32"
-        feed.image.height = "32"
+        image = feed.image
+        image.url = "http://www.kure-rad.io/wp-content/uploads/2017/11/cropped-sumika-ブルー逆-32x32.jpg"
+        image.title = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
+        image.width = "32"
+        image.height = "32"
 
-        feed.channel.itunes_summary = "呉高専の高専日誌や最近の出来事を中心に呉の今をお伝えするインターネットラジオです。"
-        feed.channel.itunes_author = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
-        feed.channel.itunes_explicit = "clean"
-        feed.channel.itunes_image = "http://www.kure-rad.io/wp-content/uploads/2017/11/ちょっくれロゴ　sumika-ブルー.jpg"
+        channel.itunes_summary = "呉高専の高専日誌や最近の出来事を中心に呉の今をお伝えするインターネットラジオです。"
+        channel.itunes_author = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
+        channel.itunes_explicit = "clean"
+        channel.itunes_image = "http://www.kure-rad.io/wp-content/uploads/2017/11/ちょっくれロゴ　sumika-ブルー.jpg"
         # feed.channel.itunes_type = "episodic"
-        feed.channel.itunes_owner.itunes_name = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
-        feed.channel.itunes_owner.itunes_email = "cho.kure.radio@gmail.com"
+        channel.itunes_owner.itunes_name = "ちょっときいて呉高専 〜呉高専の今をお伝えします〜"
+        channel.itunes_owner.itunes_email = "cho.kure.radio@gmail.com"
 
-        feed.channel.managingEditor = "cho.kure.radio@gmail.com (ちょっときいて呉高専 〜呉高専の今をお伝えします〜)"
-        feed.channel.itunes_subtitle = "ちょっときいて呉高専"
+        channel.managingEditor = "cho.kure.radio@gmail.com (ちょっときいて呉高専 〜呉高専の今をお伝えします〜)"
+        channel.itunes_subtitle = "ちょっときいて呉高専"
 
         # category = RSS::ITunesChannelModel::ITunesCategory.new("Government & Organizations")
         # category_child = RSS::ITunesChannelModel::ITunesCategory.new("Local")
         # category.itunes_categories << category_child
 
         ["Government & Organizations", "Local"].each do |category|
-          feed.channel.itunes_categories.new_category.text = category
+          channel.itunes_categories.new_category.text = category
         end
       end
 
