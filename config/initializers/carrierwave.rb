330034b53ade963b2658a624117d9f258faff600
Shintani Teppei <teppest1215@gmail.com>
2017-11-19 01:37:25 +0900

    初期setup

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production?
+  # 本番・ステージング環境はS3にUP
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY_ID'],
+        :region                => 'ap-northeast-1',
+    }
+
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_public = true
+  end
+else
+  # それ以外はローカルに保持
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+end
