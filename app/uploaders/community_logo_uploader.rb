7a4d9396c53eb48c0ce46c8901410a81a38c125a
Shintani Teppei <teppest1215@gmail.com>
2017-11-19 12:06:47 +0900

    bundle exec rubocop -a

--- app/uploaders/community_logo_uploader.rb
+++ app/uploaders/community_logo_uploader.rb
@@ -1,5 +1,4 @@
 class CommunityLogoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -46,5 +45,4 @@ class CommunityLogoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
