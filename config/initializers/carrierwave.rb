9b813f17aace003c5c93fcdea799f9540a82f0ec
Shintani Teppei <teppest1215@gmail.com>
2017-11-19 10:59:39 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 # require "carrierwave/storage/abstract"
-require "carrierwave/storage/file"
-require "carrierwave/storage/fog"
+# require "carrierwave/storage/file"
+# require "carrierwave/storage/fog"
 
 if Rails.env.production?
   # 本番・ステージング環境はS3にUP
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
-      provider: "AWS",
+      provider: "fog/aws",
       aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["S3_SECRET_KEY_ID"],
       region: "ap-northeast-1",
