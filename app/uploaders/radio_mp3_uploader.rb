5568b5a0787f0762595ff4a347315a65494f1abf
Shintani Teppei <teppest1215@gmail.com>
2017-11-19 12:29:50 +0900

    seedを修正

--- app/uploaders/radio_mp3_uploader.rb
+++ app/uploaders/radio_mp3_uploader.rb
@@ -1,5 +1,4 @@
 class RadioMp3Uploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -49,5 +48,4 @@ class RadioMp3Uploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
--- db/fixtures/40_radio.rb
+++ db/fixtures/40_radio.rb
@@ -5,7 +5,7 @@ require "faker"
     r.id = i
     r.title = Faker::Lorem.word
     r.description = Faker::Lorem.sentence
-    r.mp3 = Faker::Internet.slug(nil, "/")
+    r.mp3 = File.open(Rails.root.join("db", "fixtures", "audios", "sample.mp3"))
     r.youtube_url = Faker::Internet.url("youtube.com")
     r.podcast_url = Faker::Internet.url("podcast.com")
   end
