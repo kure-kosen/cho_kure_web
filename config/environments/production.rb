4564d8bb68fc62178aaa49fd6096ace89ce28b76
Shintani Teppei <teppest1215@gmail.com>
2017-12-03 01:00:16 +0900

    設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "cho_kure_web_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV["EC2_PRODUCTION_HOST"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
