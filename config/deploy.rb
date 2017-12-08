d345df45939d8871a3fe4d4988296a18e653c77c
taira120 <taira1999120@gmail.com>
2017-12-08 22:29:53 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:kure-kosen/cho_kure_web.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, "feature/create-staging-environment"
+set :branch, "feature/dev"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/cho_kure_web"
