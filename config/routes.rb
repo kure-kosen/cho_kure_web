c159a4c48092195d26eeda2d4b9ee3d3049e5a9a
taira120 <taira1999120@gmail.com>
2017-12-10 18:39:19 +0900

    webpackとvueをインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   mount_roboto
 
+  root to: "root#index"
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   devise_for :personalities
 
-  root to: "admin/personalities#index"
+  # root to: "admin/personalities#index"
 
   namespace :front, path: "/" do
     get "/podcast", to: "podcast#index"
