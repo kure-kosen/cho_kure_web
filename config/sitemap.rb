007b2194cbd49a2ac6f9856c3e426e471979bd9f
Shintani Teppei <teppest1215@gmail.com>
2017-12-11 12:18:50 +0900

    sitemapを生成できるようにした

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,39 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = ENV.fetch("EC2_PRODUCTION_PROTOCOL") { "" } + "://" + ENV.fetch("EC2_PRODUCTION_HOST") { "" }
 
 SitemapGenerator::Sitemap.create do
+  # Add '/personalities'
+  add "/personalities", priority: 0.7, changefreq: "daily"
+
+  # Add all personalities:
+  Personality.all.each do |personality|
+    add "/personalities/#{personality.id}", lastmod: personality.updated_at
+  end
+
+  # Add '/communities'
+  add "/communities", priority: 0.7, changefreq: "daily"
+
+  # Add all personalities:
+  Community.all.each do |community|
+    add "/communities/#{community.id}", lastmod: community.updated_at
+  end
+
+  # Add '/events'
+  add "/events", priority: 0.7, changefreq: "daily"
+
+  # Add all personalities:
+  Event.all.each do |event|
+    add "/events/#{event.id}", lastmod: event.updated_at
+  end
+
+  # Add '/radios'
+  add "/radios", priority: 0.7, changefreq: "daily"
+
+  # Add all personalities:
+  Radio.published.each do |radio|
+    add "/radios/#{radio.id}", lastmod: radio.updated_at
+  end
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
