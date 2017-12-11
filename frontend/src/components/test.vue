ed3223cf72db7de8f51dd083949ddfde27eb075c
Shintani Teppei <teppest1215@gmail.com>
2017-12-12 00:11:33 +0900

    Revert "Revert "Feature/install vue""

---
+++ frontend/src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ message }}
+    <button @click="goToRoot">go to root</button>
+    <ol v-for="i in 5"><!-- "i" is defined but never used. -->
+        <li>item</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'hoge'
+      }
+    },
+    methods: {
+      goToRoot () {
+          this.$router.push('/')
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
