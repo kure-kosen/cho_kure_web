8a14049a693fc0b60951a90937b76c76171ed073
srpkdyy <leader931334@outlook.jp>
2017-12-13 18:49:43 +0900

    delete json action from jargons_controller

--- app/controllers/admin/jargons_controller.rb
+++ app/controllers/admin/jargons_controller.rb
@@ -2,13 +2,11 @@ class Admin::JargonsController < ApplicationController
   before_action :set_jargon, only: [:show, :edit, :update, :destroy]
 
   # GET /jargons
-  # GET /jargons.json
   def index
     @jargons = Jargon.all
   end
 
   # GET /jargons/1
-  # GET /jargons/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class Admin::JargonsController < ApplicationController
   end
 
   # POST /jargons
-  # POST /jargons.json
   def create
     @jargon = Jargon.new(jargon_params)
 
     respond_to do |format|
       if @jargon.save
         format.html { redirect_to @jargon, notice: "Jargon was successfully created." }
-        format.json { render :show, status: :created, location: @jargon }
       else
         format.html { render :new }
-        format.json { render json: @jargon.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /jargons/1
-  # PATCH/PUT /jargons/1.json
   def update
     respond_to do |format|
       if @jargon.update(jargon_params)
         format.html { redirect_to @jargon, notice: "Jargon was successfully updated." }
-        format.json { render :show, status: :ok, location: @jargon }
       else
         format.html { render :edit }
-        format.json { render json: @jargon.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /jargons/1
-  # DELETE /jargons/1.json
   def destroy
     @jargon.destroy!
     respond_to do |format|
       format.html { redirect_to jargons_url, notice: "Jargon was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
