a8377ba9b2199f3eb3638773d5d2ae127a4c4c68
srpkdyy <leader931334@outlook.jp>
2017-12-13 18:09:55 +0900

    update app/models/jargon.rb

--- app/models/jargon.rb
+++ app/models/jargon.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+# == Schema Information
+# 
+# Table name: jargons
+# 
+# id          :integer        not null, primary key
+# name        :string(255)    not null
+# description :text(65535)    not null
+# radio_id    :integer        not null
+# major       :integer        not null
+
 class Jargon < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: true
+  validates :description, presence: true
+  validates :radio_id, presence: true
+  validates :major, presence: true
 end
