9626583acc1a0811b4c2cf9014f4e0bf13367719
srpkdyy <leader931334@outlook.jp>
2017-12-15 11:13:40 +0900

    fix migration

--- db/migrate/20171213083342_create_jargons.rb
+++ db/migrate/20171213083342_create_jargons.rb
@@ -3,7 +3,7 @@ class CreateJargons < ActiveRecord::Migration[5.1]
     create_table :jargons do |t|
       t.string :name, null: false
       t.text :description, null: false
-      t.references :radio, foreign_key:true, null: false
+      t.references :radio, foreign_key: true, null: false
       t.integer :major, null: false
 
       t.timestamps
