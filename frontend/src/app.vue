7f53f82e6936454b80ca2f11bdd8b0f05d450b29
taira120 <taira1999120@gmail.com>
2017-12-15 14:07:00 +0900

    install semantic-ui

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script></script>
--- frontend/src/packs/root.js
+++ frontend/src/packs/root.js
@@ -3,12 +3,16 @@ import Vue from '../../node_modules/vue'
 
 // Library
 import VueRouter from '../../node_modules/vue-router'
+import '../../node_modules/semantic-ui/dist/semantic.min.css'
+import '../../node_modules/semantic-ui/dist/semantic.min.js'
 
 Vue.use(VueRouter)
 
 
 // Component
 import App from '../app.vue'
+import Header from '../components/header.vue'
+Vue.component('app-header', Header)
 
 
 const router = new VueRouter({
