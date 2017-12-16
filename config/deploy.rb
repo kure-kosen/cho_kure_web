85c1aa223bddcdab5b0bc6b59275f7b50d3c6053
Shintani Teppei <teppest1215@gmail.com>
2017-12-16 13:02:48 +0900

    linked_dir add node_modules

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :linked_files, fetch(:linked_files, []).push(".env")
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "node_modules")
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
