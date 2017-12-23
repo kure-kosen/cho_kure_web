83e506a56be22d5398c8a8d724c78a269785179b
Shintani Teppei <teppest1215@gmail.com>
2017-12-23 16:13:38 +0900

    新着順に表示させるようにした

--- app/controllers/api/v1/radios_controller.rb
+++ app/controllers/api/v1/radios_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RadiosController < Api::V1::BaseController
   def index
-    render json: Radio.published
+    render json: Radio.published.order(published_at: :desc)
   end
 
   def show
