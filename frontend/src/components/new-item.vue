78f4a5e7ea8ba76ff5b4033c661857c22537d00b
chanyou0311 <yuukun0311@gmail.com>
2017-12-24 19:11:15 +0900

    [fix] router-link試行錯誤してみている

--- frontend/src/components/new-item.vue
+++ frontend/src/components/new-item.vue
@@ -1,57 +1,64 @@
 <template>
 <div class="item" v-bind:class="type">
-  <router-link class="image" v-bind:href="itemPath" to="/about">
+  <router-link class="image" v-bind:to="{ name: 'radios', params: { id: itemId }}">
     <img v-bind:src="imagePath">
   </router-link>
   <div class="content">
-    <a v-bind:href="itemPath" class="header">{{ title }}</a>
-    <div class="description">
-      <p>{{ description }}</p>
-    </div>
+    <router-link class="header" v-bind:to="{ name: 'radios', params: { id: itemId }}">link</router-link>
+    <div class="description" v-html="markedDescription"></div>
     <div class="meta">
       <span class="date">{{ date }}</span>
     </div>
   </div>
+  <router-link class="image" v-bind:to="{ name: 'radios', params: { id: itemId }}">link</router-link>
+  <router-link class="image" to="1">link</router-link>
+  <router-link class="item" to="/about">ちょっくれとは？</router-link>
 </div>
 </template>
 
 <script>
+var marked = require('marked');
 module.exports = {
-  props: {
-    imagePath: {
-      type: String,
-      default: 'https://placehold.jp/640x480.png',
-      required: true
+    props: {
+        imagePath: {
+            type: String,
+            default: 'https://placehold.jp/640x480.png',
+            required: true
+        },
+        itemId: {
+            type: String,
+            default: 0,
+            required: true
+        },
+        type: {
+            type: String,
+            default: 'other',
+            required: true,
+            validator: function (value) {
+                return ['radio', 'community', 'other'].includes(value)
+            }
+        },
+        title: {
+            type: String,
+            default: 'タイトルを取得できませんでした',
+            required: true,
+        },
+        description: {
+            type: String,
+            default: '詳細を取得できませんでした',
+            required: true,
+        },
+        date: {
+            type: String,
+            default: '2017/12/03 1:08',
+            required: true
+        },
     },
-    itemPath: {
-      type: String,
-      default: '/',
-      required: true
-    },
-    type: {
-      type: String,
-      default: 'other',
-      required: true,
-      validator: function (value) {
-        return ['radio', 'community', 'other'].includes(value)
-      }
-    },
-    title: {
-      type: String,
-      default: 'タイトルを取得できませんでした',
-      required: true,
-    },
-    description: {
-      type: String,
-      default: '詳細を取得できませんでした',
-      required: true,
-    },
-    date: {
-      type: String,
-      default: '2017/12/03 1:08',
-      required: true
-    },
-  }
+    computed: {
+        markedDescription: function () {
+            return marked(this.description, { sanitize: true })
+        }
+    }
 }
 </script>
 
