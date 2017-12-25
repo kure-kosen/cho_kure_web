2df6ad98fc9e80dc8720ca24c4e1a6a6aab6ce59
taira120 <taira1999120@gmail.com>
2017-12-25 13:32:07 +0900

    dried

--- app/controllers/api/v1/personalities_controller.rb
+++ app/controllers/api/v1/personalities_controller.rb
@@ -1,14 +1,21 @@
 class Api::V1::PersonalitiesController < Api::V1::BaseController
+  before_action :set_peresonality, only: %i[new_radios show]
+
   def index
     render json: Personality.on_public
   end
 
   def new_radios
-    personality = Personality.find(params[:id])
-    render json: personality.radios.published.order(published_at: :desc).take(5)
+    render json: @personality.radios.published.order(published_at: :desc).take(5)
   end
 
   def show
-    render json: Personality.find(params[:id])
+    render json: @personality
   end
+
+  private
+
+    def set_peresonality
+      @personality = Personality.find(params[:id])
+    end
 end
