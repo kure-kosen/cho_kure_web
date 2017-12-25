985e1ce666f08c68a9296640de32d97f81e765b6
taira120 <taira1999120@gmail.com>
2017-12-25 13:17:57 +0900

    バグバグしてたのでリンクにした

--- frontend/src/pages/contact.vue
+++ frontend/src/pages/contact.vue
@@ -2,10 +2,12 @@
   <div class="pusher">  
     <div class="ui container main">
       <h2 class="ui header">お問い合わせ</h2>
-      <p>以下のフォームよりご投稿ください。</p>
-      <div class="ui embed">
+      <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSeXszkj92kuwFYVY2lebh4lq5ZrnbhwUj_c3jY2WTPt1O_yqg/viewform?embedded=true">フォーム</a>よりご投稿ください。</p>
+      <!-- 
+        <div class="ui embed">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeXszkj92kuwFYVY2lebh4lq5ZrnbhwUj_c3jY2WTPt1O_yqg/viewform?embedded=true" width="760" height="1000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます...</iframe>
-      </div>
+        </div>
+        -->
     </div>
   </div>
 </template>
@@ -13,10 +15,12 @@
 <script></script>
 
 <style scoped>
+/*
 .ui.embed {
   -webkit-overflow-scrolling: touch;
   overflow-y: scroll;
 }
+*/
 
 .ui.black.segment {
   background-color: #999999;
