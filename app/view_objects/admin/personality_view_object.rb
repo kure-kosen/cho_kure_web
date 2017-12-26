d1f969541268ce20a668baf9e62f8d0f6f8518b0
Shintani Teppei <teppest1215@gmail.com>
2017-12-26 14:47:20 +0900

    権限変更をview object化した

--- app/view_objects/admin/personality_view_object.rb
+++ app/view_objects/admin/personality_view_object.rb
@@ -23,5 +23,45 @@ class Admin::PersonalityViewObject < Admin::BaseViewObject
               data: { confirm: "本当に削除してもいいですか？" },
               class: "btn btn-danger btn-sm"
     end
+
+    def link_to_change_role_to_guest(current_personality, personality)
+      return unless Pundit.policy(current_personality, [:admin, personality]).change_role_to_guest?
+      return if personality.role == PersonalityRoles::GUEST
+      link_to "ゲストに変更",
+              change_role_to_guest_admin_personality_path(personality),
+              method: :patch,
+              data: { confirm: "#{personality.name}をゲストに変更します。よろしいですか？" },
+              class: "btn btn-info btn-sm"
+    end
+
+    def link_to_change_role_to_editor(current_personality, personality)
+      return unless Pundit.policy(current_personality, [:admin, personality]).change_role_to_editor?
+      return if personality.role == PersonalityRoles::EDITOR
+      link_to "編集者に変更",
+              change_role_to_editor_admin_personality_path(personality),
+              method: :patch,
+              data: { confirm: "#{personality.name}を編集者に変更します。よろしいですか？" },
+              class: "btn btn-info btn-sm"
+    end
+
+    def link_to_change_role_to_secret(current_personality, personality)
+      return unless Pundit.policy(current_personality, [:admin, personality]).change_role_to_secret?
+      return if personality.role == PersonalityRoles::SECRET
+      link_to "シークレットに変更",
+              change_role_to_secret_admin_personality_path(personality),
+              method: :patch,
+              data: { confirm: "#{personality.name}をシークレットに変更します。よろしいですか？" },
+              class: "btn btn-info btn-sm"
+    end
+
+    def link_to_change_role_to_admin(current_personality, personality)
+      return unless Pundit.policy(current_personality, [:admin, personality]).change_role_to_guest?
+      return if personality.role == PersonalityRoles::ADMIN
+      link_to "管理者に変更",
+              change_role_to_admin_admin_personality_path(personality),
+              method: :patch,
+              data: { confirm: "#{personality.name}を管理者に変更します。よろしいですか？" },
+              class: "btn btn-info btn-sm"
+    end
   end
 end
