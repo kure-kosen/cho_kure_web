67774a68ede12933f9612aad8ce1f0acd860d7b2
Shintani Teppei <teppest1215@gmail.com>
2018-01-03 16:55:03 +0900

    fix indent

--- frontend/src/pages/top.vue
+++ frontend/src/pages/top.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="pusher">
     <div class="ui container news-contents">
-
       <h2 class="ui header">パーソナリティで調べる</h2>
       <personality-filter :personalities="personalities"></personality-filter>
-
       <h2 class="ui header">放送された回</h2>
       <div class="ui items" v-for="radio in filteredRadios">
         <radio-preview
-            :item-id="radio.id"
-            :image-path="radio.image"
-            :item-path="radio.itemPath"
-            type="radio"
-            :title="radio.title"
-            :description="radio.description"
-            :personalities="radio.personalities"
-            :mp3-url="radio.mp3.url"
-            :date="radio.published_at">
+          :item-id="radio.id"
+          :image-path="radio.image"
+          :item-path="radio.itemPath"
+          type="radio"
+          :title="radio.title"
+          :description="radio.description"
+          :personalities="radio.personalities"
+          :mp3-url="radio.mp3.url"
+          :date="radio.published_at">
         </radio-preview>
       </div>
     </div>
