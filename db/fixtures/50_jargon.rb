bfcd637698d5f2441ef553dc5b5764eb59099158
Shintani Teppei <teppest1215@gmail.com>
2018-01-05 18:49:04 +0900

    bug fix

--- db/fixtures/50_jargon.rb
+++ db/fixtures/50_jargon.rb
@@ -2,7 +2,7 @@ radio_ids = Radio.pluck(:id)
 (1..50).each do |i|
   Jargon.seed do |j|
     j.id = i
-    j.name = %w[機械 電気 環境 建築] + "科の" + %w[専門用語 俗語] + " " + rand(1..50).to_s
+    j.name = %w[機械 電気 環境 建築].sample + "科の" + %w[専門用語 俗語].sample + " " + i.to_s
     j.description = "hoge" * rand(1..20) + "fuga" * rand(1..20)
     j.radio_id = radio_ids.sample
     j.major = JargonMajors.values.sample
