536ab5090c2bfad82ffc90c9144f12f06b9e6a7e
chanyou0311 <yuukun0311@gmail.com>
2018-01-23 18:27:27 +0900

    [add] シェアボタンの追加

---
+++ frontend/src/components/share-buttons.vue
@@ -0,0 +1,39 @@
+t<template>
+<div>
+  <div class="ui divider"></div>
+  <a class="ui image mini"
+     v-bind:href="'http://www.facebook.com/share.php?u='+fullUrl"
+     rel="nofollow"
+     onclick="window.open(this.href, 'fbwindow', 'width=500, height=500, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;">
+    <img src="../../images/share_buttons/share_fb.png">
+  </a>
+  <a class="ui image mini"
+     v-bind:href="'https://twitter.com/share?url='+fullUrl+'&via=cho_kure&related=cho_kure&hashtags=ちょっくれ&text='+''"
+     rel="nofollow"
+     target="_blank"
+     onclick="window.open(this.href, 'twwindow', 'width=500, height=500, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;">
+    <img src="../../images/share_buttons/share_tw.png">
+  </a>
+  <a class="ui image mini"
+     v-bind:href="'http://line.me/R/msg/text/?'+fullUrl"
+     rel="nofollow"
+     target="_blank"
+     onclick="window.open(this.href, 'linewindow', 'width=500, height=500, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;">
+    <img src="../../images/share_buttons/share_line.png">
+  </a>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function () {
+        return {
+            fullUrl: window.location.protocol + window.location.host + this.$route.path,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
