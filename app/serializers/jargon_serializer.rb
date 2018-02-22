82183efd363e7f57c009006581dd2dabc5db30a5
srpkdyy <leader931334@outlook.jp>
2018-02-22 23:53:31 +0900

    update jargon_serializer

--- app/serializers/jargon_serializer.rb
+++ app/serializers/jargon_serializer.rb
@@ -1,3 +1,15 @@
 class JargonSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :description, :radio_id, :major, :created_at, :updated_at
+
+  def description
+    MarkdownHelper.markdown(object.description)
+  end
+
+  def created_at
+    object.created_at.to_s(:datetime)
+  end
+
+  def updated_at
+    object.updated_at.to_s(:datetime)
+  end
 end
