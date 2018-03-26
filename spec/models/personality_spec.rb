c09c9c3ff151499531fabd67c1a73a495bb9e431
Shintani Teppei <teppest1215@gmail.com>
2018-03-26 19:25:55 +0900

    add personality model spec

--- spec/models/personality_spec.rb
+++ spec/models/personality_spec.rb
@@ -1,5 +1,71 @@
 require "rails_helper"
 
 RSpec.describe Personality, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#member?" do
+    let(:guest) { create(:personality, :guest) }
+    let(:editor) { create(:personality, :editor) }
+    let(:secret) { create(:personality, :secret) }
+    let(:admin) { create(:personality, :admin) }
+
+    it "return boolean correctly" do
+      expect(guest.member?).to be false
+      expect(editor.member?).to be true
+      expect(secret.member?).to be true
+      expect(admin.member?).to be true
+    end
+  end
+
+  describe "#allow_change_role?" do
+    let(:guest) { create(:personality, :guest) }
+    let(:editor) { create(:personality, :editor) }
+    let(:secret) { create(:personality, :secret) }
+    let(:admin) { create(:personality, :admin) }
+
+    it "return boolean correctly" do
+      expect(guest.allow_change_role?).to be false
+      expect(editor.allow_change_role?).to be false
+      expect(secret.allow_change_role?).to be true
+      expect(admin.allow_change_role?).to be true
+    end
+  end
+
+  describe "#change_role_to_admin!" do
+    let(:guest) { create(:personality, :guest) }
+
+    it "change role to admin" do
+      expect(guest.admin?).to be false
+      guest.change_role_to_admin!
+      expect(guest.admin?).to be true
+    end
+  end
+
+  describe "#change_role_to_secret!" do
+    let(:guest) { create(:personality, :guest) }
+
+    it "change role to secret" do
+      expect(guest.secret?).to be false
+      guest.change_role_to_secret!
+      expect(guest.secret?).to be true
+    end
+  end
+
+  describe "#change_role_to_editor!" do
+    let(:guest) { create(:personality, :guest) }
+
+    it "change role to editor" do
+      expect(guest.editor?).to be false
+      guest.change_role_to_editor!
+      expect(guest.editor?).to be true
+    end
+  end
+
+  describe "#change_role_to_guest!" do
+    let(:editor) { create(:personality, :editor) }
+
+    it "change role to guest" do
+      expect(editor.guest?).to be false
+      editor.change_role_to_guest!
+      expect(editor.guest?).to be true
+    end
+  end
 end
