faf75ce5298f376d38e4b2b9d1ba0d49f643f7d3
Shintani Teppei <teppest1215@gmail.com>
2018-06-01 08:41:00 +0900

    rails側からbundleされたファイルを読み込めるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def webpack_javascript_include_tag(path)
+    path = "#{path}.js" unless path.ends_with?('.js')
+    path = if ENV["USE_WEBPACK_DEV_SERVER"].to_b
+      "/assets/build/#{path}"
+    else
+      # TODO(euglena1215): implementation for production
+      "/assets/build/#{path}"
+    end
+
+    javascript_include_tag(path)
+  end
 end
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "../app/assets/javascripts/webpack"),
+    path: path.resolve(__dirname, "../public/assets/build"),
     filename: "[name].js",
   },
 
