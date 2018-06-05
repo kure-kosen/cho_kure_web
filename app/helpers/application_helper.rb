7b865b5509c2e896558a3291c3b0f7cc0d7e60d8
Shintani Teppei <teppest1215@gmail.com>
2018-06-05 16:56:40 +0900

    rubocop -a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def webpack_javascript_include_tag(path)
-    path = "#{path}.js" unless path.ends_with?('.js')
+    path = "#{path}.js" unless path.ends_with?(".js")
     path = if ENV["USE_WEBPACK_DEV_SERVER"].to_b
-      "/assets/build/#{path}"
-    else
-      # TODO(euglena1215): implementation for production
-      "/assets/build/#{path}"
-    end
+             "/assets/build/#{path}"
+           else
+             # TODO(euglena1215): implementation for production
+             "/production/to/#{path}"
+           end
 
     javascript_include_tag(path)
   end
