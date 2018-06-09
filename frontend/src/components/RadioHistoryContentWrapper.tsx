1f174400c8f23d46656fd6aba6f7180651115439
Shintani Teppei <teppest1215@gmail.com>
2018-06-09 13:23:49 +0900

    Add wireframe

---
+++ frontend/src/components/RadioHistoryContentWrapper.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export const RadioHistoryContentWrapper = () => <Wrapper>radio-history-content-wrapper</Wrapper>;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 1000px;
+  color: white;
+  background-color: #2c5379;
+`;
