3bfe9076746421460ee30e1dfd139fe6be283166
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-24 03:14:54 +0900

    間をいい感じにするやつを汎用化

--- frontend/src/commons/style.ts
+++ frontend/src/commons/style.ts
@@ -20,6 +20,19 @@ export const title = `
   color: #00afec;
 `;
 
+export const goodSpace = `
+display: box;
+display: -webkit-box;
+display: -moz-box;
+display: -o-box;
+display: -ms-box;
+box-pack: justify;
+-moz-box-pack: justify;
+-webkit-box-pack: justify;
+-o-box-pack: justify;
+-ms-box-pack: justify;
+`;
+
 injectGlobal`
   *, *:before, *:after {
     -webkit-box-sizing: border-box;
