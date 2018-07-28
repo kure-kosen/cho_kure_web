a7b4aa3925dd63013c23ea49bab4e8db9e90338f
yTakezoe <y.zoeshi.1999@gmail.com>
2018-07-28 09:36:20 +0900

    add TweetStream

--- frontend/src/components/TweetStream.tsx
+++ frontend/src/components/TweetStream.tsx
@@ -1,11 +1,25 @@
 import * as React from "react";
-import styled from "styled-components";
+//import styled from "styled-components";
+import { Timeline } from "react-twitter-widgets";
 
-export const TweetStream = () => <TweetStreamStyle>tweet-stream</TweetStreamStyle>;
+export const TweetStream = () => (
+  <Timeline
+    dataSource = {{
+      sourceType: 'profile',
+      screenName: 'cho_kure'
+    }}
+    options = {{
+      //username: 'cho_kure',
+      width: '100%',
+      height: '400px',
+      chrome: 'noheader nofooter noborders'
+    }}
+  />
+);
 
-const TweetStreamStyle = styled.div`
+/*const TweetStreamStyle = styled.div`
   width: 100%;
   color: gray;
   background-color: #fff200;
   height: 400px;
-`;
+`;*/
