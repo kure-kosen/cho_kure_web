2ab5e9f5eae223d65345b2a8b5dcb7612d16cc9e
yTakezoe <y.zoeshi.1999@gmail.com>
2018-07-28 10:13:50 +0900

    delete unnecessary comment

--- frontend/src/components/TweetStream.tsx
+++ frontend/src/components/TweetStream.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-//import styled from "styled-components";
 import { Timeline } from "react-twitter-widgets";
 
 export const TweetStream = () => (
@@ -9,17 +8,9 @@ export const TweetStream = () => (
       screenName: 'cho_kure'
     }}
     options = {{
-      //username: 'cho_kure',
       width: '100%',
-      height: '400px',
+      height: '400',
       chrome: 'noheader nofooter noborders'
     }}
   />
 );
-
-/*const TweetStreamStyle = styled.div`
-  width: 100%;
-  color: gray;
-  background-color: #fff200;
-  height: 400px;
-`;*/
