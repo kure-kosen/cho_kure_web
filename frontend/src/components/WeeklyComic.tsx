c6719890604d2f713568fe38c7ab791c549c6509
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-08-04 10:07:30 +0900

    [add] Weekly Comicの表示

--- frontend/src/components/WeeklyComic.tsx
+++ frontend/src/components/WeeklyComic.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import styled from "styled-components";
 
-export const WeeklyComic = () => <WeeklyComicStyle>weekly-comic</WeeklyComicStyle>;
+import * as comic from "./../../images/WeeklyComic.jpg";
+
+export const WeeklyComic = () => (
+  <WeeklyComicStyle>
+    <PictureArea />
+  </WeeklyComicStyle>
+);
 
 const WeeklyComicStyle = styled.div`
   width: 100%;
   color: white;
   background-color: #333333;
   height: 600px;
+`;
+
+const PictureArea = styled.div`
+  background-image: url(${comic});
+  width: 100%;
+  height: 100%;
 `;
