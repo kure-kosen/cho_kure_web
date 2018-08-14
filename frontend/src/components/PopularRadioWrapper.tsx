5f8dff26534e7667fc082b903bd80f390d95be70
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-08-14 14:18:30 +0900

    show radio card

--- frontend/src/components/PopularRadioWrapper.tsx
+++ frontend/src/components/PopularRadioWrapper.tsx
@@ -5,9 +5,23 @@ import { title } from "./../commons/style";
 import { ChkButtonBase } from "./../commons/ChkButtonBase";
 import { chkColors } from "./../commons/color";
 
+import * as radioCardImg from "./../../images/radiocard_img.png"
+
 export const PopularRadioWrapper = () => (
   <PopularRadioWrapperStyle>
     <Title>popular radio</Title>
+    
+    <PopularRadioCardWrapper>
+      <PopularRadioCard>
+        <RadioImg src={radioCardImg}/>
+        <RadioText>
+          <RadioTitle>飲食店バグハンター</RadioTitle>
+          <RadioDescript>
+            最近やっていること、Ruby25周年、else if、コードゴルフ…
+          </RadioDescript>
+        </RadioText>
+      </PopularRadioCard>
+    </PopularRadioCardWrapper>
     
     <PopularRadioMoreButton>more</PopularRadioMoreButton>
   </PopularRadioWrapperStyle>
@@ -24,6 +38,34 @@ const Title = styled.div`
   ${title};
   color: ${chkColors.blue};
   padding: 0 20px;
+`;
+
+const PopularRadioCardWrapper = styled.div `
+  width: auto;
+  height: auto;
+`;
+
+const RadioText = styled.span `
+`;
+const PopularRadioCard = styled.div `
+  width: 100%;
+  display: flex;
+`;
+
+const RadioDescript = styled.p`
+  width: auto;
+  height: auto;
+  color: ${chkColors.gray};
+`;
+
+const RadioImg = styled.img`
+  width: auto;
+  height: 100px;
+`;
+
+const RadioTitle = styled.div `
+  font-size: 18px;
+  color: ${chkColors.orange};
 `;
 
 const PopularRadioMoreButton = ChkButtonBase.extend`
