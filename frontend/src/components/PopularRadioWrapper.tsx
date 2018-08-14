45759a75e06865b381ca9945dd18101e84faae6f
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-08-14 11:29:42 +0900

    set Title and more button

--- frontend/src/components/PopularRadioWrapper.tsx
+++ frontend/src/components/PopularRadioWrapper.tsx
@@ -1,11 +1,37 @@
 import * as React from "react";
 import styled from "styled-components";
 
-export const PopularRadioWrapper = () => <PopularRadioWrapperStyle>popular-radio-wrapper</PopularRadioWrapperStyle>;
+import { title } from "./../commons/style";
+import { ChkButtonBase } from "./../commons/ChkButtonBase";
+import { chkColors } from "./../commons/color";
+
+export const PopularRadioWrapper = () => (
+  <PopularRadioWrapperStyle>
+    <Title>popular radio</Title>
+    
+    <PopularRadioMoreButton>more</PopularRadioMoreButton>
+  </PopularRadioWrapperStyle>
+);
 
 const PopularRadioWrapperStyle = styled.div`
   width: 100%;
-  color: black;
-  background-color: #f1f0ee;
+  color: white;
+  background-color: ${chkColors.white};
   height: 400px;
+`;
+
+const Title = styled.div`
+  ${title};
+  color: ${chkColors.blue};
+  padding: 0 20px;
+`;
+
+const PopularRadioMoreButton = ChkButtonBase.extend`
+  background-color: ${chkColors.orange};
+  width: 90px;
+  height: 48px;
+  margin-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 20px;
 `;
