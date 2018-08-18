7b418a574c27ad62370d06ec16a205b4c2dd2743
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-18 14:27:53 +0900

    annotated comic_serializer

--- app/serializers/comic_serializer.rb
+++ app/serializers/comic_serializer.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: comics
+#
+#  id         :integer          not null, primary key
+#  image      :string(255)      not null
+#  radio_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_comics_on_radio_id  (radio_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (radio_id => radios.id)
+#
+
 class ComicSerializer < ActiveModel::Serializer
   attributes :id, :image, :created_at, :updated_at
 
