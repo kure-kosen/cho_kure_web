8c26cc313a4d6ff1f89f17cdb407dbb0e7ae42d3
kobakakzu0429 <kobakazu10@gmail.com>
2018-08-18 14:27:01 +0900

    has_one random comic image

--- db/fixtures/70_comic.rb
+++ db/fixtures/70_comic.rb
@@ -4,6 +4,6 @@ raise StandardError, "production環境では利用できません" if Rails.env.
   Comic.seed do |c|
     c.id = i
     c.radio = Radio.all.sample
-    c.image = File.open(Rails.root.join("db", "fixtures", "images", "comic_01.jpg"))
+    c.image = File.open(Rails.root.join("db", "fixtures", "images", "comic_0#{[*1..3].sample}.jpg"))
   end
 end
