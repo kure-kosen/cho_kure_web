bb48b478bba8ecb5f5efd86db8aceb93ffb6cbc7
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-03 16:55:01 +0900

    switch url by case(prod, dev)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
   def webpack_javascript_include_tag(path)
     path = "#{path}.js" unless path.ends_with?(".js")
+    path = assets_path(path)
     path = if ENV["USE_WEBPACK_DEV_SERVER"].to_b
-             "/assets/build/#{path}"
+             "http://localhost:5000/#{path}"
            else
-             # TODO(euglena1215): implementation for production
-             "/production/to/#{path}"
+             "/assets/build/#{path}"
            end
 
-    javascript_include_tag(path)
+    javascript_include_tag(assets_path(path))
+  end
+
+  def assets_path(path)
+    return path if ENV["USE_WEBPACK_DEV_SERVER"].to_b
+
+    manifest = Rails.application.config.assets_manifest
+    manifest && manifest[path].present? ? manifest[path] : path
   end
 end
