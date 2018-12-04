436fd9990967f2ab10dbf7c5ac224b1f9375cb24
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-04 17:06:25 +0900

    if not file exist, error happen

--- config/initializers/assets_manifest.rb
+++ config/initializers/assets_manifest.rb
@@ -1,4 +1,11 @@
 Rails.application.config.assets_manifest =
   if File.exist?(Rails.root.join("public", "assets", "build", "manifest.json"))
     JSON.parse(File.read(Rails.root.join("public", "assets", "build", "manifest.json")))
+  else
+    raise "\n
+      +--------------------------------------------------+
+      | public/assets/build/manifest.jsonが存在しません. |
+      | $ yarn build を実行してください.                 |
+      +--------------------------------------------------+
+    "
   end
