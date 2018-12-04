d87f2710d38b18cf876f90408c088c6edd892c9b
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-04 13:45:56 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,16 @@
 module ApplicationHelper
   def webpack_javascript_include_tag(path)
     path = "#{path}.js" unless path.ends_with?(".js")
-    path = assets_path(path)
     path = if ENV["USE_WEBPACK_DEV_SERVER"].to_b
              "http://localhost:5000/#{path}"
            else
-             "/assets/build/#{path}"
+             "/assets/build/#{find_hash_name(path)}"
            end
 
-    javascript_include_tag(assets_path(path))
+    javascript_include_tag(path)
   end
 
-  def assets_path(path)
-    return path if ENV["USE_WEBPACK_DEV_SERVER"].to_b
-
+  def find_hash_name(path)
     manifest = Rails.application.config.assets_manifest
     (manifest && manifest[path].present?) ? manifest[path] : path
   end
