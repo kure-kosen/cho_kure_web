f026fbf8500a90918cc1effda4e2582f180ee710
kobakakzu0429 <kobakazu10@gmail.com>
2018-12-04 19:18:06 +0900

    fix

--- config/initializers/assets_manifest.rb
+++ config/initializers/assets_manifest.rb
@@ -2,10 +2,12 @@ Rails.application.config.assets_manifest =
   if File.exist?(Rails.root.join("public", "assets", "build", "manifest.json"))
     JSON.parse(File.read(Rails.root.join("public", "assets", "build", "manifest.json")))
   else
-    raise "\n
-      +--------------------------------------------------+
-      | public/assets/build/manifest.jsonが存在しません. |
-      | $ yarn build を実行してください.                 |
-      +--------------------------------------------------+
-    "
+    if !ENV["USE_WEBPACK_DEV_SERVER"].to_b
+      raise "\n
+        +--------------------------------------------------+
+        | public/assets/build/manifest.jsonが存在しません. |
+        | $ yarn build を実行してください.                 |
+        +--------------------------------------------------+
+      "
+    end
   end
