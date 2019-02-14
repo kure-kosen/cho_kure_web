48c5db0b6cbe9a269f985388d07d604d3a35a75d
kobakakzu0429 <kobakazu10@gmail.com>
2019-02-14 15:44:11 +0900

    ReRreate ChkButtonBase

--- frontend/src/commons/ChkButtonBase.tsx
+++ frontend/src/commons/ChkButtonBase.tsx
@@ -1,7 +1,35 @@
-import { css } from "styled-components";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
 import { chkColors } from "./color";
 
-export const ChkButtonBase = css`
+interface IPropsChkButtonBase {
+  to?: string;
+  text: string | React.ReactNode;
+  color?: string;
+}
+
+interface IBackgroundColor {
+  color?: string;
+}
+
+const ChkButtonBase = (props: IPropsChkButtonBase) => {
+  if (props.to) {
+    if (props.to.startsWith("http")) {
+      return <ATag href={props.to}>{props.text}</ATag>;
+    } else {
+      return (
+        <LinkTag color={props.color} to={props.to}>
+          {props.text}
+        </LinkTag>
+      );
+    }
+  } else {
+    return <ButtonTag>{props.text}</ButtonTag>;
+  }
+};
+
+const ButtonStyle = css`
   display: block;
   width: 100%;
   margin: 0;
@@ -11,6 +39,20 @@ export const ChkButtonBase = css`
   border-style: none;
   box-shadow: 4px 3px 10px 0px ${chkColors.shadow};
   border-radius: 20px;
-  background-color: ${chkColors.orange};
   text-align: center;
+  background-color: ${(props: IBackgroundColor) => (props.color ? chkColors[props.color] : chkColors.orange)};
 `;
+
+const ButtonTag = styled.button`
+  ${ButtonStyle}
+`;
+
+const LinkTag = styled(Link)`
+  ${ButtonStyle}
+`;
+
+const ATag = styled.a`
+  ${ButtonStyle}
+`;
+
+export default ChkButtonBase;
