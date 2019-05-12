184b76a367f74a59ff6319bb6e157ce1201ab4a6
kobakakzu0429 <kobakazu10@gmail.com>
2019-05-12 13:59:31 +0900

    lint

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,21 +1,34 @@
-import {setConsoleOptions} from '@storybook/addon-console';
-import {withKnobs} from '@storybook/addon-knobs/react';
-import {addDecorator, addParameters, configure} from '@storybook/react';
-import React from 'react';
+import * as React from "react";
 
-import GlobalStyle from '../src/commons/style';
+import { addDecorator, addParameters, configure } from "@storybook/react";
+
+import { setConsoleOptions } from "@storybook/addon-console";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs/react";
+
+import { MemoryRouter } from "react-router";
+import GlobalStyle from "../src/commons/style";
 
 addParameters({
   backgrounds: [
-    { name: 'white', value: '#fff', default: true },
-    { name: '100%', value: '#fff' },
-    { name: '80%', value: '#ccc' },
-    { name: '60%', value: '#999' },
-    { name: '40%', value: '#666' },
-    { name: '20%', value: '#333' },
-    { name: '0%', value: '#000' },
+    { name: "white", value: "#fff", default: true },
+    { name: "100%", value: "#fff" },
+    { name: "80%", value: "#ccc" },
+    { name: "60%", value: "#999" },
+    { name: "40%", value: "#666" },
+    { name: "20%", value: "#333" },
+    { name: "0%", value: "#000" }
   ]
 });
+
+addDecorator(story => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>);
+addDecorator(withGlobalStyles);
+addDecorator(withKnobs);
+addDecorator(
+  withInfo({
+    inline: true
+  })
+);
 
 setConsoleOptions({ panelExclude: [] });
 
@@ -28,11 +41,8 @@ function withGlobalStyles(storyFn) {
   );
 }
 
-addDecorator(withGlobalStyles);
-addDecorator(withKnobs);
-
 function loadStories() {
-  const req = require.context('../src/stories/', true, /\.stories\.(tsx|js)$/);
+  const req = require.context("../src/stories/", true, /\.stories\.(tsx|js)$/);
   req.keys().forEach(filename => req(filename));
 }
 
--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,22 +1,22 @@
 const path = require("path");
 
-module.exports = ({config}) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader'),
-        options: {configFileName: './tsconfig.json'}
+        loader: require.resolve("awesome-typescript-loader"),
+        options: { configFileName: "./tsconfig.json" }
       },
-      {loader: require.resolve('react-docgen-typescript-loader')},
+      { loader: require.resolve("react-docgen-typescript-loader") },
       {
-        loader: require.resolve('@storybook/addon-storysource/loader'),
-        options: {parser: 'typescript'}
+        loader: require.resolve("@storybook/addon-storysource/loader"),
+        options: { parser: "typescript" }
       }
     ]
   });
 
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.extensions.push(".ts", ".tsx");
 
   return config;
 };
