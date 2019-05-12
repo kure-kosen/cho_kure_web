0b9521ad4f3f9c07e58a1f58f067a18608af8290
kobakakzu0429 <kobakazu10@gmail.com>
2019-05-12 10:42:38 +0900

    introduce storybook

---
+++ frontend/stories/index.stories.js
@@ -0,0 +1,11 @@
+import {storiesOf} from '@storybook/react';
+import {Button} from '@storybook/react/demo';
+import React from 'react';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button>Hello Button</Button>
+  ))
+  .add('with emoji', () => (
+    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ));
