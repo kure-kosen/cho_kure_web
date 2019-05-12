d9a298c38179d9fb338bb78219ae357ad6c7e583
kobakakzu0429 <kobakazu10@gmail.com>
2019-05-12 12:13:14 +0900

    add fakers

--- frontend/src/stories/FeaturedBlog.stories.tsx
+++ frontend/src/stories/FeaturedBlog.stories.tsx
@@ -1,39 +1,27 @@
 import * as React from "react";
 import { MemoryRouter } from "react-router";
 
-// import {action} from '@storybook/addon-actions';
 import { withInfo } from "@storybook/addon-info";
-import { withKnobs } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
+
+import { text10, text30, text100, text200 } from "./fakers/text";
 
 import { FeaturedBlog } from "../components/FeaturedBlog";
 
 storiesOf("FeaturedBlog", module)
-  .addDecorator(withKnobs)
   .addDecorator(withInfo({ inline: true }))
   .addDecorator(story => <MemoryRouter initialEntries={["/", "posts"]}>{story()}</MemoryRouter>)
   .add("default", () => (
     <FeaturedBlog
       title="news title"
       date="YYYY/MM/DD"
-      summary="人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。
-        人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。"
+      summary="人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。"
       author="だれかさん"
       to="/blog"
     />
+  ))
+  .add("ShortText", () => <FeaturedBlog title={text10} date="YYYY/MM/DD" summary={text30} author={text10} to="/blog" />)
+  .add("LongText", () => <FeaturedBlog title={text30} date="YYYY/MM/DD" summary={text100} author={text30} to="/blog" />)
+  .add("VeryLongText", () => (
+    <FeaturedBlog title={text30} date="YYYY/MM/DD" summary={text200} author={text30} to="/blog" />
   ));
-
-// IProps {
-// title: string;
-// date: string;
-// summary: string;
-// author: string;
-// to: string;
-// }
-
-// title:"news title"
-// date:"YYYY/MM/DD"
-// summary:"人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。
-// 人の世界は住みにくい人の世界は住みにくい人の世界は住みにくい。"
-// author:"だれかさん"
-// to:"/blog"
