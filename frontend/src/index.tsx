c2c1136b32506cca513b7126c78290e19586f620
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-09 12:46:58 +0900

    remove mobx-react & replace mobx-react-lite

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,21 +3,16 @@ import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "mobx-react";
 import GlobalStyle from "@/constants/styles/global";
 
 import App from "@/components/App";
-import RootStore from "@/stores/RootStore";
 
-// TODO(euglena1215): URLのproductionとの切り替え方法を考える
 ReactDOM.render(
-  <Provider rootStore={new RootStore("http://localhost:3000")}>
-    <BrowserRouter basename="/app">
-      <>
-        <App />
-        <GlobalStyle />
-      </>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter basename="/app">
+    <>
+      <App />
+      <GlobalStyle />
+    </>
+  </BrowserRouter>,
   document.getElementById("app")
 );
--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import RootContext from "@/utils/Contexts/RootContext";
+
 import Index from "@/pages/Index";
 import RadioHistory from "@/pages/RadioHistory";
 import Contact from "@/pages/Contact";
 import Blog from "@/pages/Blog";
 import Personality from "@/pages/Personality";
 
-export default () => (
-  <main>
-    <Switch>
-      <Route exact path="/" component={Index} />
-      <Route path="/radios" component={RadioHistory} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/personality" component={Personality} />
-    </Switch>
-  </main>
-);
+export default () => {
+  const rootStore = React.useContext(RootContext);
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route
+          path="/radios"
+          render={() => <RadioHistory rootStore={rootStore} />}
+        />
+        <Route
+          path="/contact"
+          render={() => <Contact rootStore={rootStore} />}
+        />
+        <Route path="/blog" component={Blog} />
+        <Route path="/personality" component={Personality} />
+      </Switch>
+    </main>
+  );
+};
