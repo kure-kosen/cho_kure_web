6e28a15a507b5af2d9853ae5f8ae254a9eb99334
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-09 13:37:10 +0900

    add

--- frontend/src/components/molecules/RadioCard/RadioCardWrapper.tsx
+++ frontend/src/components/molecules/RadioCard/RadioCardWrapper.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-import card from "./radio-card.png";
+import { IRadio } from "@/api/RadioApi";
+import { color } from "@/constants/styles";
 
-export default () => <RadioCardWrapperStyle />;
+import CardImage from "@/components/atoms/RadioCard/RadioCardImage";
+import CardContent from "@/components/atoms/RadioCard/RadioCardContent";
+import PlayButton from "@/components/atoms/RadioCard/RadioCardPlayButton";
+
+export default (props: IRadio) => {
+  const { title, description, mp3, image, play_time } = props;
+
+  return (
+    <RadioCardWrapperStyle>
+      <CardImage image={image} />
+      <PlayButton mp3={mp3} play_time={play_time} />
+      <CardContent title={title} description={description} />
+    </RadioCardWrapperStyle>
+  );
+};
 
 const RadioCardWrapperStyle = styled.div`
-  width: 282px;
+  width: 280px;
   height: 445px;
-  background-image: url(${card});
-  background-size: cover;
+  border-radius: 8px;
+  border: 3px solid ${color.SKY_BLUE};
+  position: relative;
 `;
