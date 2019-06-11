e692dddef02a85ef8a678a47db71e4db2e78eb97
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-12 00:56:40 +0900

    add useAudio

---
+++ frontend/src/utils/hooks/useAudio.ts
@@ -0,0 +1,34 @@
+import React from "react";
+// import console = require("console");
+
+export default (url: string) => {
+  const [audio] = React.useState(new Audio(url));
+  const [, _forceUpdate] = React.useState(false);
+  const forceUpdate = () => _forceUpdate(prevState => !prevState);
+
+  React.useEffect(() => {
+    audio.addEventListener("play", forceUpdate);
+    audio.addEventListener("pause", forceUpdate);
+    audio.addEventListener("ended", forceUpdate);
+    audio.addEventListener("timeupdate", forceUpdate);
+
+    return () => {
+      audio.removeEventListener("play", forceUpdate);
+      audio.removeEventListener("pause", forceUpdate);
+      audio.removeEventListener("ended", forceUpdate);
+      audio.addEventListener("timeupdate", forceUpdate);
+    };
+  }, []);
+
+  const play = () => audio.play();
+  const pause = () => audio.pause();
+  const jump = (value: number) => (audio.currentTime = value);
+
+  return {
+    isPlay: audio.paused,
+    play,
+    pause,
+    jump,
+    times: { currentTime: audio.currentTime, duration: audio.duration }
+  };
+};
