69f0cb8b5ab8e8204cbe2a44561ee80708316647
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-13 21:08:04 +0900

    add sample seekbar

--- frontend/src/components/atoms/RadioCard/RadioCardPlayButton.tsx
+++ frontend/src/components/atoms/RadioCard/RadioCardPlayButton.tsx
@@ -7,25 +7,48 @@ import { color } from "@/constants/styles";
 import useAudio from "@/utils/hooks/useAudio";
 import PlayButtonProgress from "@/components/atoms/RadioCard/RadioCardPlayButtonProgress";
 
-export default (props: Pick<IRadio, "mp3">) => {
-  const { mp3 } = props;
-  const { isPlay, play, pause, jump, times } = useAudio(mp3.url!);
+export default (props: Pick<IRadio, "mp3" | "play_time">) => {
+  const { mp3, play_time } = props;
+  const { isPlay, play, pause, jump, times } = useAudio({
+    url: mp3.url!,
+    play_time
+  });
+  const [currentTime, setCurrentTime] = React.useState(0);
+  const seekBarRef = React.useRef<HTMLDivElement>(null);
+
+  const hoge = React.useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    const mouse = e.pageX;
+    const element = seekBarRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const position = rect.left + window.pageXOffset;
+    const offset = mouse - position;
+    const width = rect.right - rect.left;
+  }, []);
 
   return (
     <Wrapper>
       <PlayButtonProgressWrapper>
         <ButtonWrapper>
-          {isPlay ? (
+          {!isPlay ? (
             <PlayButton className="fas fa-play" onClick={play} />
           ) : (
             <PauseButton className="fas fa-pause" onClick={pause} />
           )}
         </ButtonWrapper>
         <PlayButtonProgress
-          progress={(times.currentTime / times.duration) * 100}
+          progress={(() => {
+            const percent =
+              Math.round((times.currentTime / times.duration) * 1000) / 10;
+            return isNaN(percent) ? 0 : percent;
+          })()}
+          currentTime={times.currentTime}
         />
         {/* <p onClick={() => jump(2)}>jump</p> */}
       </PlayButtonProgressWrapper>
+      <SeekBar ref={seekBarRef} onClick={hoge} />
     </Wrapper>
   );
 };
@@ -36,7 +59,7 @@ const Wrapper = styled.div`
   left: 0;
   right: 0;
   margin: 0 auto;
-  width: 38px;
+  width: 100%;
   height: 38px;
 `;
 
@@ -65,3 +88,10 @@ const PlayButton = styled(ButtonBase)`
 const PauseButton = styled(ButtonBase)``;
 
 const PlayButtonProgressWrapper = styled.div``;
+
+const SeekBar = styled.div`
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;
+  background: linear-gradient(#ccc, #ccc) no-repeat #eee;
+`;
