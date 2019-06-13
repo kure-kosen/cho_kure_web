fc18aa3d2054a9471e207bec0a51d5d65bb382bc
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-13 21:05:03 +0900

    add play_time

--- frontend/src/components/molecules/RadioCard/RadioCardWrapper.tsx
+++ frontend/src/components/molecules/RadioCard/RadioCardWrapper.tsx
@@ -9,12 +9,12 @@ import CardContent from "@/components/atoms/RadioCard/RadioCardContent";
 import PlayButton from "@/components/atoms/RadioCard/RadioCardPlayButton";
 
 export default (props: IRadio) => {
-  const { title, description, mp3, image } = props;
+  const { title, description, mp3, play_time, image } = props;
 
   return (
     <RadioCardWrapperStyle>
       <CardImage image={image} />
-      <PlayButton mp3={mp3} />
+      <PlayButton mp3={mp3} play_time={play_time} />
       <CardContent title={title} description={description} />
     </RadioCardWrapperStyle>
   );
