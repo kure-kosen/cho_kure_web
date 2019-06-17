1b4a1065d8dbbda940b868d9c402ea1d3b16d7f5
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-17 23:03:33 +0900

    fix

--- frontend/src/stores/RadioStore.ts
+++ frontend/src/stores/RadioStore.ts
@@ -3,13 +3,12 @@ import { action, observable } from "mobx";
 import RadioApi, { IRadio } from "@/api/RadioApi";
 
 export default class RadioStore {
-  @observable public radios: IRadio[];
+  @observable public radios?: IRadio[];
 
   public transportLayer: RadioApi;
 
   constructor(transportLayer: RadioApi) {
     this.transportLayer = transportLayer;
-    this.radios = [];
   }
 
   public async fetchRadios() {
