daa3ffea2a55f253adcac07e94fd64a613451d9f
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-24 22:27:27 +0900

    create sns icons

---
+++ frontend/src/components/atoms/Icons/CircleIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+import { color } from "@/constants/styles";
+
+interface IProps {
+  children: string | React.HTMLAttributes<HTMLElement> | React.ReactNode;
+}
+
+export default (props: IProps) => <Circle>{props.children}</Circle>;
+
+const Circle = styled.div`
+  float: left;
+  font-size: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  vertical-align: middle;
+  margin-right: 10px;
+  border-radius: 50%;
+  background-color: ${color.ORANGE};
+  color: rgba(255, 255, 255, 0.8);
+`;
---
+++ frontend/src/components/atoms/Icons/TwitterIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Circle from "@/components/atoms/Icons/CircleIcon";
+
+export default () => (
+  <Circle>
+    <i className="fab fa-twitter fa-fw" />
+  </Circle>
+);
