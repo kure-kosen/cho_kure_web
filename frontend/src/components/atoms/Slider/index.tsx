b6d5fbe8c58672f832fe06423b2e6d32408e771f
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-25 21:44:51 +0900

    fix arrow in slider

--- frontend/src/components/atoms/Slider/index.tsx
+++ frontend/src/components/atoms/Slider/index.tsx
@@ -5,16 +5,22 @@ import "slick-carousel/slick/slick.css";
 import "./ChkSlider.css";
 
 export interface IProps {
-  InnerComponent: any;
+  children: string | HTMLElement | React.ReactNode;
 }
 
 export default (props: IProps) => (
-  <Slider {...settings}>{props.InnerComponent}</Slider>
+  <Slider {...settings}>{props.children}</Slider>
 );
 
-const ChkNextArrow = (props: any) => <div onClick={props.onClick} />;
+const ChkNextArrow = (props: any) => {
+  const { className, onClick } = props;
+  return <div className={className} onClick={onClick} />;
+};
 
-const ChkPrevArrow = (props: any) => <div onClick={props.onClick} />;
+const ChkPrevArrow = (props: any) => {
+  const { className, onClick } = props;
+  return <div className={className} onClick={onClick} />;
+};
 
 const settings = {
   dots: false,
