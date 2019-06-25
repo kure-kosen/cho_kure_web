c3186f4701e61b4e3b5a105728aa8421245f469a
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-25 21:59:25 +0900

    add regularPersonality & get shuffledRegularPersonality

--- frontend/src/stores/PersonalityStore.ts
+++ frontend/src/stores/PersonalityStore.ts
@@ -4,12 +4,28 @@ import PersonalityApi, { IPersonality } from "@/api/PersonalityApi";
 
 export default class PersonalityStore {
   @observable public personalities: IPersonality[];
+  @observable public regularPersonalities: IPersonality[];
 
   public transportLayer: PersonalityApi;
 
   constructor(transportLayer: PersonalityApi) {
     this.transportLayer = transportLayer;
     this.personalities = [];
+    this.regularPersonalities = [];
+  }
+
+  public get shuffledRegularPersonality() {
+    const shuffledPersonality = [...this.regularPersonalities];
+    for (let i = 0, len = shuffledPersonality.length; i < len; i++) {
+      const rand = Math.floor(Math.random() * i);
+
+      [shuffledPersonality[i], shuffledPersonality[rand]] = [
+        shuffledPersonality[rand],
+        shuffledPersonality[i]
+      ];
+    }
+
+    return shuffledPersonality;
   }
 
   public async fetchPersonalities() {
@@ -18,7 +34,21 @@ export default class PersonalityStore {
     this.setPersonalities(res.data);
   }
 
-  @action public setPersonalities(personalities: IPersonality[]) {
+  public async fetchRegularPersonality() {
+    if (this.regularPersonalities && this.regularPersonalities.length > 0) {
+      return;
+    }
+    const res = await this.transportLayer.fetchRegularPersonality();
+    this.setRegularPersonalities(res.data);
+  }
+
+  @action
+  public setPersonalities(personalities: IPersonality[]) {
     this.personalities = personalities;
+  }
+
+  @action
+  public setRegularPersonalities(personalities: IPersonality[]) {
+    this.regularPersonalities = personalities;
   }
 }
