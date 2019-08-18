ef14a288df53e206955550479cb3058179d24014
kobakakzu0429 <kobakazu10@gmail.com>
2019-08-18 11:13:53 +0900

    create personality api & store

--- frontend/src/stores/RootStore.ts
+++ frontend/src/stores/RootStore.ts
@@ -1,12 +1,15 @@
-import ContactStore from "@/stores/ContactStore";
 import ContactApi from "@/api/ContactApi";
-import RadioStore from "@/stores/RadioStore";
+import PersonalityApi from "@/api/PersonalityApi";
 import RadioApi from "@/api/RadioApi";
 import RestClient from "@/api/RestClient";
+import ContactStore from "@/stores/ContactStore";
+import PersonalityStore from "@/stores/PersonalityStore";
+import RadioStore from "@/stores/RadioStore";
 
 export default class RootStore {
   public contactStore: ContactStore;
   public radioStore: RadioStore;
+  public personalityStore: PersonalityStore;
 
   constructor(baseUrl: string) {
     const restClient = new RestClient(baseUrl);
@@ -16,5 +19,8 @@ export default class RootStore {
 
     const radioApi = new RadioApi(restClient);
     this.radioStore = new RadioStore(radioApi);
+
+    const personalityApi = new PersonalityApi(restClient);
+    this.personalityStore = new PersonalityStore(personalityApi);
   }
 }
