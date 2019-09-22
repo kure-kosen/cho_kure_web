0099fbe5e9bed0b0c32ad0d124d01be8f6bb2ae4
kobakakzu0429 <kobakazu10@gmail.com>
2019-09-22 13:31:19 +0900

    using tsx

---
+++ frontend/stories/1-Button.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@storybook/react/demo";
+
+export default {
+  title: "Button"
+};
+
+export const text = () => (
+  <Button onClick={action("clicked")}>Hello Button</Button>
+);
+
+export const emoji = () => (
+  <Button onClick={action("clicked")}>
+    <>😀 😎 👍 💯</>
+  </Button>
+);
---
+++ frontend/stories/2-hoge.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default {
+  title: "aaaaaaaaa"
+};
+
+export const normal: React.FC = () => {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+};
