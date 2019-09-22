88fcc036e60e3a438e51ca4db5ace73c21358c6b
kobakakzu0429 <kobakazu10@gmail.com>
2019-09-23 00:25:40 +0900

    fix webpack config

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,20 +1,29 @@
+'use strict'
+
+const path = require('path');
+
 module.exports = ({config}) => {
   config.module.rules.push(
       {
         test: /\.(ts|tsx)?$/,
-        use: [
-          {loader: require.resolve('ts-loader')},
-          {loader: require.resolve('react-docgen-typescript-loader')}
-        ],
+        loader: require.resolve('ts-loader'),
         exclude: /node_modules/,
         options: {transpileOnly: true}
-      }  // {
-         //   test: /\.stories\.tsx?$/,
-         //   loader: require.resolve('@storybook/source-loader'),
-         //   options: { injectParameters: true },
-         //   enforce: 'pre',
-         // }
-  );
-  config.resolve.extensions.push('.ts', '.tsx');
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: require.resolve('react-docgen-typescript-loader'),
+        exclude: /node_modules/
+      });
+  // {
+  //   test: /\.stories\.tsx?$/,
+  //   loader: require.resolve('@storybook/source-loader'),
+  //   options: { injectParameters: true },
+  //   enforce: 'pre',
+  // }
+
+  config.resolve.extensions.push('.js', '.ts', '.tsx');
+  config.resolve.alias = {'@': path.resolve(__dirname, '../src')};
+
   return config;
 };
