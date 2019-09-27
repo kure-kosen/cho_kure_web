479783b1c88bfcc5b3bd0beeb80e60c848933874
kobakakzu0429 <kobakazu10@gmail.com>
2019-09-27 15:07:04 +0900

    add @storybook/addon-storyshots-puppeteer

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   verbose: true,
-  setupFiles: ['<rootDir>/.jest/register-context.ts'],
-  testMatch: ['**/*.test.(ts|tsx)'],
+  setupFiles: ["<rootDir>/.jest/register-context.ts"],
+  testMatch: ["**/*.test.(ts|tsx)"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/.jest/fileMock.js',
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/.jest/fileMock.js",
+    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules"
   }
 };
--- frontend/stories/Storyshot.test.ts
+++ frontend/stories/Storyshot.test.ts
@@ -1,3 +1,4 @@
 import initStoryshots from "@storybook/addon-storyshots";
+import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
 
-initStoryshots();
+initStoryshots({ suite: "Image storyshots", test: imageSnapshot() });
