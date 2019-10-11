1f1070dd6236959223b6b43a8eaf7bc428b41a96
kobakakzu0429 <kobakazu10@gmail.com>
2019-10-11 21:17:18 +0900

    update circleci

--- frontend/stories/Storyshot.test.ts
+++ frontend/stories/Storyshot.test.ts
@@ -1,11 +1,32 @@
 import initStoryshots from "@storybook/addon-storyshots";
-import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
+import {
+  imageSnapshot,
+  CustomConfig
+} from "@storybook/addon-storyshots-puppeteer";
 
 const storybookUrl = process.env.CIRCLECI
   ? "file:///home/circleci/repo/frontend/storybook-static"
   : "http://localhost:6006/";
 
+const beforeScreenshot: CustomConfig["beforeScreenshot"] = (
+  _page,
+  _context
+) => {
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, 600)
+  );
+};
+
+const getMatchOptions: CustomConfig["getMatchOptions"] = _context => ({
+  // Allow 5% difference
+  failureThreshold: 0.05,
+  failureThresholdType: "percent",
+  customDiffConfig: { threshold: 0.15 }
+});
+
 initStoryshots({
   suite: "Image storyshots",
-  test: imageSnapshot({ storybookUrl })
+  test: imageSnapshot({ storybookUrl, beforeScreenshot, getMatchOptions })
 });
