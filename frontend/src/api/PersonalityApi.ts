86c0432215c0c7da30da977ebc81251ad32ac7aa
kobakakzu0429 <kobakazu10@gmail.com>
2019-10-12 14:49:38 +0900

    add personality api

--- frontend/src/api/PersonalityApi.ts
+++ frontend/src/api/PersonalityApi.ts
@@ -4,11 +4,14 @@ import { IRadio } from "@/api/RadioApi";
 export interface IPersonality {
   id: number;
   name: string;
-  nickname: string;
+  nickname?: string | null;
   description: string;
   image: string;
   tag_list: string[];
   radios?: Exclude<IRadio, "personalities">;
+  facebook_id?: string | null;
+  instagram_id?: string | null;
+  twitter_id?: string | null;
 }
 
 export default class PersonalityApi {
@@ -20,5 +23,9 @@ export default class PersonalityApi {
 
   public fetchPersonality() {
     return this.restClient.get<IPersonality[]>("/api/v1/personalities");
+  }
+
+  public fetchRegularPersonality() {
+    return this.restClient.get<IPersonality[]>("/api/v1/personalities/regular");
   }
 }
--- frontend/src/stores/PersonalityStore.ts
+++ frontend/src/stores/PersonalityStore.ts
@@ -4,6 +4,7 @@ import PersonalityApi, { IPersonality } from "@/api/PersonalityApi";
 
 export default class PersonalityStore {
   @observable public personalities?: IPersonality[];
+  @observable public regularPersonalities?: IPersonality[];
 
   public transportLayer: PersonalityApi;
 
@@ -11,10 +12,39 @@ export default class PersonalityStore {
     this.transportLayer = transportLayer;
   }
 
+  public get shuffledRegularPersonality() {
+    if (!this.regularPersonalities) return;
+    if (this.regularPersonalities.length > 0) return;
+
+    const shuffledPersonality = [...this.regularPersonalities];
+    for (let i = 0, len = shuffledPersonality.length; i < len; i++) {
+      const rand = Math.floor(Math.random() * i);
+
+      [shuffledPersonality[i], shuffledPersonality[rand]] = [
+        shuffledPersonality[rand],
+        shuffledPersonality[i]
+      ];
+    }
+
+    return shuffledPersonality;
+  }
+
   public async fetchPersonalities() {
     if (this.personalities && this.personalities.length > 0) return;
     const res = await this.transportLayer.fetchPersonality();
     this.setPersonalities(res.data);
+  }
+
+  public async fetchRegularPersonality() {
+    if (this.regularPersonalities && this.regularPersonalities.length > 0) {
+      return;
+    }
+    const res = await this.transportLayer.fetchRegularPersonality();
+    this.setRegularPersonalities(res.data);
+  }
+
+  @action public setRegularPersonalities(personalities: IPersonality[]) {
+    this.regularPersonalities = personalities;
   }
 
   @action public setPersonalities(personalities: IPersonality[]) {
