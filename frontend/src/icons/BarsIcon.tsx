ca3f807e4a623344df2b8087812df9b0e780608b
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-05 13:41:19 +0900

    add BarsIcon

---
+++ frontend/src/icons/BarsIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { IconWrapper } from "@/icons/IconWrapper";
+
+export const BarsIcon = () => (
+  <IconWrapper>
+    <i className="fas fa-bars fa-fw" />
+  </IconWrapper>
+);
--- frontend/src/icons/index.tsx
+++ frontend/src/icons/index.tsx
@@ -1,5 +1,6 @@
 import { FacebookIcon } from "./FacebookIcon";
 import { InstagramIcon } from "./InstagramIcon";
 import { TwitterIcon } from "./TwitterIcon";
+import { BarsIcon } from "./BarsIcon";
 
-export { FacebookIcon, InstagramIcon, TwitterIcon };
+export { FacebookIcon, InstagramIcon, TwitterIcon, BarsIcon };
