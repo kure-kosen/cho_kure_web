a24c7879413176d9e552dbddc889453b1097e1a5
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-06 10:52:34 +0900

    add normal BarsIcon

--- frontend/src/icons/BarsIcon.tsx
+++ frontend/src/icons/BarsIcon.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import styled from "styled-components";
 import { CircleIconWrapper } from "@/icons/CircleIconWrapper";
+import { color } from "@/constants/styles";
 
 export const BarsCircleIcon = () => (
   <CircleIconWrapper>
     <i className="fas fa-bars fa-fw" />
   </CircleIconWrapper>
 );
+
+export const BarsIcon = () => (
+  <StyledIcon>
+    <i className="fas fa-bars fa-fw" />
+  </StyledIcon>
+);
+
+const StyledIcon = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  vertical-align: middle;
+  color: ${color.BLUE};
+`;
--- frontend/src/icons/index.tsx
+++ frontend/src/icons/index.tsx
@@ -1,11 +1,12 @@
 import { FacebookCircleIcon } from "./FacebookIcon";
 import { InstagramCircleIcon } from "./InstagramIcon";
 import { TwitterCircleIcon } from "./TwitterIcon";
-import { BarsCircleIcon } from "./BarsIcon";
+import { BarsCircleIcon, BarsIcon } from "./BarsIcon";
 
 export {
   FacebookCircleIcon,
   InstagramCircleIcon,
   TwitterCircleIcon,
-  BarsCircleIcon
+  BarsCircleIcon,
+  BarsIcon
 };
