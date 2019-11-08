0c39b12acafa4fed410abb805eb43dc262122f25
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-08 19:35:26 +0900

    add route: RadioDetail

--- frontend/src/layouts/Main.tsx
+++ frontend/src/layouts/Main.tsx
@@ -8,6 +8,7 @@ import RadioHistory from "@/pages/RadioHistory";
 import Contact from "@/pages/Contact";
 import Blog from "@/pages/Blog";
 import Personality from "@/pages/Personality";
+import { RadioDetail } from "@/pages/RadioDetail";
 
 export default () => {
   const rootStore = React.useContext(RootContext);
@@ -17,15 +18,23 @@ export default () => {
       <Switch>
         <Route exact path="/" render={() => <Index rootStore={rootStore} />} />
         <Route
+          exact
           path="/radios"
           render={() => <RadioHistory rootStore={rootStore} />}
         />
         <Route
+          exact
+          path="/radios/:radioId"
+          render={() => <RadioDetail rootStore={rootStore} />}
+        />
+        <Route
+          exact
           path="/contact"
           render={() => <Contact rootStore={rootStore} />}
         />
-        <Route path="/blog" component={Blog} />
+        <Route exact path="/blog" component={Blog} />
         <Route
+          exact
           path="/personality"
           render={() => <Personality rootStore={rootStore} />}
         />
