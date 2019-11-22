bd0787448954c15514328780c9bad85ea0eaaee6
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-22 13:56:02 +0900

    refactor

--- frontend/src/pages/RadioDetail.tsx
+++ frontend/src/pages/RadioDetail.tsx
@@ -15,80 +15,20 @@ import { RadioPlayer } from "@/components/molecules/RadioDetails/RadioPlayer";
 import Personalities from "@/components/molecules/Personalities";
 import { Share } from "@/components/molecules/RadioDetails/Share";
 import CircleSpinner from "@/components/atoms/Spinners/CircleSpinner";
-
-const MOCK_DATA = {
-  id: 13,
-  title: "#13 飲食店バグハンター",
-  description:
-    "最近やっていること、Ruby25周年、else if、コードゴルフ、ちょっくれサイトの仕組み、IoTなどの話をしました。",
-  mp3: {
-    url:
-      "https://cho-kure-web-s3.s3.amazonaws.com/uploads/radio/mp3/13/ck013.mp3"
-  },
-  image:
-    "https://cho-kure-web-s3.s3.amazonaws.com/uploads/radio/image/13/thumb_ck013.png",
-  playTime: "32:52",
-  duration: 1972,
-  personalities: [
-    {
-      id: 2,
-      name: "金子 平",
-      nickname: "",
-      description:
-        "サーバー側の開発を担当してます。\n初の本科生パーソナリティの枠はいただきました！！！ ",
-      image:
-        "https://cho-kure-web-s3.s3.amazonaws.com/uploads/personality/image/2/thumb_IMG_0880_copy.JPG",
-      tagList: ["本科生", "電気科", "開発", "冬眠中"]
-    },
-    {
-      id: 3,
-      name: "中村 優",
-      nickname: "ちゃんゆー",
-      description:
-        "こんにちは。ちゃんゆーだよ！\nフロントエンド開発やTwitter解析とかをしてるよ！ わーい！",
-      image:
-        "https://cho-kure-web-s3.s3.amazonaws.com/uploads/personality/image/3/thumb_img_7944_1024.jpg",
-      tagList: ["専攻科", "開発", "広報"]
-    },
-    {
-      id: 7,
-      name: "梶 凌太",
-      nickname: "",
-      description:
-        "バックエンドの開発担当。\nどんなことも、まずやってみる。\nやる気が出るのは、やり始めてから！",
-      image:
-        "https://cho-kure-web-s3.s3.amazonaws.com/uploads/personality/image/7/thumb_s__25108524.jpg",
-      tagList: ["本科生", "電気科", "開発"]
-    },
-    {
-      id: 14,
-      name: "小畠 一泰",
-      nickname: "こばくん",
-      description:
-        "Web Frontend Engineerをやりながら地図作ったり、ペットボトル飛ばしたりする人です",
-      image:
-        "https://cho-kure-web-s3.s3.amazonaws.com/uploads/personality/image/14/thumb_83JHKvBf_400x400.jpg",
-      tagList: ["本科生", "電気科", "開発"]
-    }
-  ],
-  comic: null
-};
+import RadioStore from "@/stores/RadioStore";
+import { IRadio } from "@/api/RadioApi";
 
 interface Props {
-  radioStore: any;
-  SHARE_URL: string;
-  setRadio: React.Dispatch<any>;
-  radio: any;
-  radioId: number;
+  radioStore: RadioStore;
+  setRadio: React.Dispatch<IRadio | null>;
+  radio: IRadio | null;
 }
 
-const Main: React.FC<Props> = ({
-  radioStore,
-  SHARE_URL,
-  setRadio,
-  radio,
-  radioId
-}) => {
+const Main: React.FC<Props> = ({ radioStore, setRadio, radio }) => {
+  const location = useLocation();
+  const SHARE_URL = CHK.FRONT_END.PROD + location.pathname;
+  const { radioId } = useParams();
+
   if (radio) {
     return (
       <>
@@ -113,7 +53,7 @@ const Main: React.FC<Props> = ({
   }
 
   throw (async () => {
-    const cache = await radioStore.fetchRadio(radioId);
+    const cache = await radioStore.fetchRadio(Number(radioId));
     setRadio(cache);
   })();
 };
@@ -121,16 +61,14 @@ const Main: React.FC<Props> = ({
 export const RadioDetail = observer((props: { rootStore: RootStore }) => {
   const { rootStore } = props;
   const { radioStore } = rootStore;
-  const { radioId } = useParams();
 
-  const [radio, setRadio] = useState<any>(null);
-
-  const location = useLocation();
-  const SHARE_URL = CHK.FRONT_END.PROD + location.pathname;
+  const [radio, setRadio] = useState<IRadio | null>(null);
 
   return (
     <div>
-      <RadioDetailHeroArea>{MOCK_DATA.title}</RadioDetailHeroArea>
+      <Suspense fallback={<CircleSpinner />}>
+        <RadioDetailHeroArea>{radio ? radio.title : ""}</RadioDetailHeroArea>
+      </Suspense>
       <Contrainer>
         <Sidebar>
           <PopularRadioWrapper />
@@ -138,13 +76,7 @@ export const RadioDetail = observer((props: { rootStore: RootStore }) => {
         </Sidebar>
         <MainContentWrapper>
           <Suspense fallback={<CircleSpinner />}>
-            <Main
-              SHARE_URL={SHARE_URL}
-              radio={radio}
-              setRadio={setRadio}
-              radioStore={radioStore}
-              radioId={Number(radioId)}
-            />
+            <Main radio={radio} setRadio={setRadio} radioStore={radioStore} />
           </Suspense>
         </MainContentWrapper>
       </Contrainer>
