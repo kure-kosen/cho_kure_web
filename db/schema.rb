ada61789b710ea8f2c431fe20b39f31d3e3f7380
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-23 14:08:11 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191101051212) do
+ActiveRecord::Schema.define(version: 20191123050423) do
 
   create_table "communities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -93,8 +93,8 @@ ActiveRecord::Schema.define(version: 20191101051212) do
     t.string "twitter_id"
     t.string "facebook_id"
     t.string "instagram_id"
-    t.string "affiliation_name"
-    t.string "affiliation_details"
+    t.string "organization"
+    t.string "position"
     t.index ["email"], name: "index_personalities_on_email", unique: true
     t.index ["reset_password_token"], name: "index_personalities_on_reset_password_token", unique: true
   end
