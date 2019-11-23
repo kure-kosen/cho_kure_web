073a43047a811e11d590fed47114737e7d45c3f3
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-23 11:15:50 +0900

    refactor

--- frontend/src/api/ContactApi.test.ts
+++ frontend/src/api/ContactApi.test.ts
@@ -1,6 +1,7 @@
 import MockAdapter from "axios-mock-adapter";
 import RestClient from "@/api/RestClient";
 import ContactApi from "@/api/ContactApi";
+import { expectResponces } from "./expectResponces";
 
 const baseUrl = "http://localhost:3000";
 const csrfToken = "this-is-csrf-token";
@@ -18,40 +19,12 @@ describe("ContactApi", () => {
   });
 
   it("fetchContactEnum in Mock", async () => {
-    const expectResponce = {
-      corners: {
-        普通のお便り: 0,
-        "ラジオへの感想・意見": 10,
-        ラジオ出演: 20,
-        バグ報告: 30
-      },
-      departments: {
-        機械工学科: 10,
-        電気情報工学科: 20,
-        環境都市工学科: 30,
-        建築学科: 40,
-        専攻科: 50,
-        卒業生: 60,
-        保護者: 70,
-        教員: 80,
-        その他: 90
-      },
-      grades: {
-        "1年": 10,
-        "2年": 20,
-        "3年": 30,
-        "4年": 40,
-        "5年": 50,
-        obog: 60,
-        その他: 70
-      }
-    };
     const expectHttpStatusCode = 200;
     mockAxios
       .onGet("/api/v1/contacts/enum")
-      .reply(expectHttpStatusCode, expectResponce);
+      .reply(expectHttpStatusCode, expectResponces.api.v1.contacts.enum);
     const res = await contactApi.fetchContactEnum();
-    expect(res.data).toStrictEqual(expectResponce);
+    expect(res.data).toStrictEqual(expectResponces.api.v1.contacts.enum);
     expect(res.status).toBe(expectHttpStatusCode);
   });
 
