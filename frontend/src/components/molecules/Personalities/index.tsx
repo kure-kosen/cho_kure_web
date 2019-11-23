04a72e5316383689e67caff07db1a82b0592fa42
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-24 00:11:48 +0900

    supported for mobile in PersonalityPage

--- frontend/src/components/molecules/Personalities/index.tsx
+++ frontend/src/components/molecules/Personalities/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+import { device } from "@/constants/styles";
 import { IPersonality } from "@/api/PersonalityApi";
 import CircleSpinner from "@/components/atoms/Spinners/CircleSpinner";
 import { PersonalityCard } from "@/components/molecules/Personalities/Card";
 import { TileCardsWrapper } from "@/components/molecules/Cards/TileCardsWrapper";
+import { PersonalityProfileMiniCard } from "@/components/atoms/FeaturedPersonality/PersonalityProfileMiniCard";
 
-interface IProps {
+interface Props {
   personalities: IPersonality[];
 }
 
-export default (props: IProps) => {
-  const { personalities } = props;
-
+export default ({ personalities }: Props) => {
   if (!personalities) {
     return (
       <Wrapper>
@@ -22,15 +22,32 @@ export default (props: IProps) => {
   }
 
   return (
-    <TileCardsWrapper>
-      {personalities.map(personality => (
-        <PersonalityCard key={personality.id} {...personality} />
-      ))}
-    </TileCardsWrapper>
+    <>
+      <device.Default>
+        <TileCardsWrapper>
+          {personalities.map(personality => (
+            <PersonalityCard key={personality.id} {...personality} />
+          ))}
+        </TileCardsWrapper>
+      </device.Default>
+      <device.ForMobile>
+        <MobileWrapper>
+          {personalities.map(({ id, name, image }) => (
+            <PersonalityProfileMiniCard key={id} name={name} image={image} />
+          ))}
+        </MobileWrapper>
+      </device.ForMobile>
+    </>
   );
 };
 
 const Wrapper = styled.div`
   width: 100%;
   height: auto;
+`;
+
+const MobileWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 15px;
 `;
