ee351b187260c8fb3350c177be27bcf0d65f4528
kobakakzu0429 <kobakazu10@gmail.com>
2019-12-29 13:21:58 +0900

    change ip or localhost

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,10 +1,10 @@
 export const CHK = {
   BACK_END: {
-    DEV: "http://localhost:3000",
+    DEV: `http://${process.env.IP_ADDRESS}:3000/`,
     PROD: "https://www.kure-rad.io"
   },
   FRONT_END: {
-    DEV: "http://localhost:3000/app",
+    DEV: `http://${process.env.IP_ADDRESS}:3000/app`,
     PROD: "https://www.kure-rad.io/app"
   }
 };
