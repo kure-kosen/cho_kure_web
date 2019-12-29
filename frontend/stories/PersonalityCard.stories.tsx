b07761dddef0e9a98383afde92495d7ec450e8aa
kobakakzu0429 <kobakazu10@gmail.com>
2019-12-29 14:03:39 +0900

    add story

--- frontend/stories/PersonalityCard.stories.tsx
+++ frontend/stories/PersonalityCard.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PersonalityCard } from "@/components/molecules/Personalities/Card";
 import { PersonalityProfileMiniCard } from "@/components/atoms/FeaturedPersonality/PersonalityProfileMiniCard";
+import { PersonalityMobileCard } from "@/components/molecules/Personalities/MobileCard";
 import { chanyou } from "./mocks/api/personality";
 
 export default {
@@ -22,4 +23,10 @@ export const longText = () => <PersonalityCard {...longTextSource} />;
 export const normalMini = () => <PersonalityProfileMiniCard {...chanyou} />;
 export const longTextMini = () => (
   <PersonalityProfileMiniCard {...longTextSource} />
+);
+export const nomalMobile = () => (
+  <PersonalityMobileCard personality={chanyou} />
+);
+export const longTextMobile = () => (
+  <PersonalityMobileCard personality={longTextSource} />
 );
