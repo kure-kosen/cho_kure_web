b8b3390f18b5deb8837b34b561196307f522547c
kobakakzu0429 <kobakazu10@gmail.com>
2019-12-31 22:33:24 +0900

    create ChkTheme

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,57 @@
+export type ThemeName = "chk";
+
+// export interface Theme {
+//   color: {
+//     brand: {
+//       darkPrimary: string;
+//       lightPrimary: string;
+//       primary: string;
+//     };
+//     text: {
+//       primary: string;
+//       secondary: string;
+//     };
+//     accent: string;
+//     divider: string;
+//   };
+// }
+
+export interface Theme {
+  color: {
+    orange: string;
+    vividOrange: string;
+    blue: string;
+    skyBlue: string;
+    black: string;
+    gray: string;
+    shadow: string;
+    white: string;
+    placeholder: string;
+    aqua: string;
+    darkAqua: string;
+    error: string;
+    disabled: string;
+  };
+}
+
+const chkTheme: Theme = {
+  color: {
+    orange: "#ECB73F",
+    vividOrange: "#E59134",
+    blue: "#00AFEC",
+    skyBlue: "#A8E1E3",
+    black: "#565A62",
+    gray: "#7f8287",
+    shadow: "#14141433",
+    white: "#ffffff",
+    placeholder: "#b0bec5",
+    aqua: "#b3dfe2",
+    darkAqua: "#50AAB7",
+    error: "#ff4b42",
+    disabled: "#cccccc"
+  }
+};
+
+export const themes = {
+  chk: chkTheme
+};
